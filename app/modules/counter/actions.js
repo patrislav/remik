a106b6fb3ef016e7d9377b17858bc400c1815901
Patryk Kalinowski <patrislav7@gmail.com>
2016-11-02 23:13:45 +0100

    Remove old counter

--- app/modules/counter/actions.js
+++
@@ -1,7 +0,0 @@
-import { actionCreator } from '../../lib/rxstate'
-
-const action = actionCreator('counter')
-
-export const increment$ = action('increment')
-export const decrement$ = action('decrement')
-export const reset$ = action('reset')
--- app/modules/index.js
+++ app/modules/index.js
@@ -1,9 +1,8 @@
 import {combineReducerStreams} from '../lib/rxstate'
 
-import counter from './counter/reducer'
 import chat from './chat/reducer'
 import users from './users/reducer'
 
 export const { rootReducer$, initialState } = combineReducerStreams({
-  counter, chat, users
+  chat, users
 })
